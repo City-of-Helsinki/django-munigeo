f3928b8e459897f0ca5fc73e69a130b45c875aba
Ville Koivunen <ville.koivunen@hel.fi>
2018-11-07 15:34:28 +0200

    Bump version to v0.2.11

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-munigeo',
-    version='0.2.10',
+    version='0.2.11',
     packages=['munigeo'],
     include_package_data=True,
     license='BSD License',
