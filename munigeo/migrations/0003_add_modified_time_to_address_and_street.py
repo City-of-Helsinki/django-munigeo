8ffc386bca40b6e6da62f5bb24d2597c8428915b
Timo Tuominen <tituomin@gmail.com>
2019-09-18 12:42:35 +0300

    Fix migration which caused timezone warnings

--- munigeo/migrations/0003_add_modified_time_to_address_and_street.py
+++ munigeo/migrations/0003_add_modified_time_to_address_and_street.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import datetime
 
 
 class Migration(migrations.Migration):
@@ -15,13 +14,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='address',
             name='modified_at',
-            field=models.DateTimeField(default=datetime.datetime(1970, 1, 1, 2, 0), help_text='Time when the information was last changed', auto_now=True),
+            field=models.DateTimeField(help_text='Time when the information was last changed', auto_now=True),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='street',
             name='modified_at',
-            field=models.DateTimeField(default=datetime.datetime(1970, 1, 1, 2, 0), help_text='Time when the information was last changed', auto_now=True),
+            field=models.DateTimeField(help_text='Time when the information was last changed', auto_now=True),
             preserve_default=False,
         ),
     ]
