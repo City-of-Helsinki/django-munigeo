634b02ed7617da4ce41f800835ea03b5e47c2456
Mika Hietanen <m.hietanen84@gmail.com>
2021-02-05 13:41:33 +0200

    Import resident parking areas
    Also update project version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-munigeo',
-    version='0.2.47',
+    version='0.2.48',
     packages=['munigeo'],
     include_package_data=True,
     license='BSD License',
