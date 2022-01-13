859b0330a7d8f2d6ffac91b93b96f56c95ac5f0d
Mika Hietanen <m.hietanen84@gmail.com>
2022-01-13 11:14:42 +0200

    Update translations-module and project version

--- munigeo/translation.py
+++ munigeo/translation.py
@@ -1,14 +1,24 @@
 from modeltranslation.translator import translator, TranslationOptions
-from munigeo.models import AdministrativeDivision, Municipality, Street
+from munigeo.models import Address, AdministrativeDivision, Municipality, Street
+
 
 class AdministrativeDivisionTranslationOptions(TranslationOptions):
     fields = ('name',)
-translator.register(AdministrativeDivision, AdministrativeDivisionTranslationOptions)
+
 
 class MunicipalityTranslationOptions(TranslationOptions):
     fields = ('name',)
-translator.register(Municipality, MunicipalityTranslationOptions)
+
 
 class StreetTranslationOptions(TranslationOptions):
     fields = ('name',)
+
+
+class AddressTranslationOptions(TranslationOptions):
+    fields = ('full_name',)
+
+
+translator.register(AdministrativeDivision, AdministrativeDivisionTranslationOptions)
+translator.register(Municipality, MunicipalityTranslationOptions)
 translator.register(Street, StreetTranslationOptions)
+translator.register(Address, AddressTranslationOptions)
