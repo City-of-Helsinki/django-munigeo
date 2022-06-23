795421c0915553bf8a8e33f6b4886ba1eb60ab6c
juuso-j <juuso.jokiniemi@turku.fi>
2022-06-23 09:01:21 +0300

    Add naturalsort function

---
+++ munigeo/migrations/0013_add_naturalsort_function.py
@@ -0,0 +1,28 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("munigeo", "0010_add_postalcodearea"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+                create or replace function naturalsort(text)
+                    returns bytea
+                    language sql
+                    immutable strict
+                as $f$
+                    select string_agg(convert_to(coalesce(r[2],
+                    length(length(r[1])::text) || length(r[1])::text || r[1]),
+                    'SQL_ASCII'),'\\x00')
+                    from regexp_matches($1, '0*([0-9]+)|([^0-9]+)', 'g') r;
+                $f$;            
+            """,
+            reverse_sql="""
+                drop function naturalsort;
+            """,
+        ),
+    ]
