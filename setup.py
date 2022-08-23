fb6005175e1f0e12b726c4af0f9747ad40761a12
Tero Virtanen <tero.virtanen@mavericks.fi>
2022-08-23 09:27:02 +0300

    set version to 0.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-munigeo',
-    version='0.0.0',
+    version='0.4.0',
     packages=['munigeo'],
     include_package_data=True,
     license='BSD License',
