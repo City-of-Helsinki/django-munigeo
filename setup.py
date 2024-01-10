bad1e0afb29e17318724dfb4c3ec1f1e7e303a33
Mika Hietanen <m.hietanen84@gmail.com>
2024-01-10 13:08:57 +0200

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-munigeo',
-    version='0.2.78',
+    version='0.2.80',
     packages=['munigeo'],
     include_package_data=True,
     license='BSD License',
